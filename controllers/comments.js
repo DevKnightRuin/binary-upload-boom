2b29d5d8e2dce59c5b2d952905940d9366e29f7d
Nicholas Naquin <DevKnightRuin@outlook.com>
2023-01-04 20:01:09 -0600

    create and view comments on posts

---
+++ controllers/comments.js
@@ -0,0 +1,21 @@
+const Post = require("../models/Post")
+const Comment = require("../models/Comment")
+
+
+module.exports = {
+
+    createComment: async (req, res) => {
+        console.log(' hi i got this far')
+        try {
+            await Comment.create({
+                message: req.body.comment,
+                likes: 0,
+                post: req.params.id
+            });
+            console.log("Created comment");
+            res.redirect(`/post/${req.params.id}`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
--- middleware/multer.js
+++ middleware/multer.js
@@ -4,9 +4,9 @@ const path = require("path");
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
+    let ext = path.extname(file.originalname).toLowerCase();
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
+      cb(new Error("File type is not supported. Your attempted to upload a : " + ext), false);
       return;
     }
     cb(null, true);
